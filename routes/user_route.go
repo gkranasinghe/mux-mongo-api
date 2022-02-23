dcd74afc7517a7a37b47dfb74b0da34415599bb1
gkranasinghe <gkranasinghe@gmail.com>
2022-02-23 17:10:54 +0530

    add route for put

--- routes/user_route.go
+++ routes/user_route.go
@@ -8,4 +8,5 @@ import (
 func UserRoute(router *mux.Router) {
     router.HandleFunc("/user", controllers.CreateUser()).Methods("POST") //add this
 	router.HandleFunc("/user/{userId}", controllers.GetAUser()).Methods("GET") //add this
+	router.HandleFunc("/user/{userId}", controllers.EditAUser()).Methods("PUT") //add this
 }
