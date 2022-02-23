cb1ca0872f254d42a45d6cc92839beb092347fb7
gkranasinghe <gkranasinghe@gmail.com>
2022-02-23 17:01:25 +0530

    modify to get details of a user in  user_controller.go

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -12,6 +12,8 @@ import (
     "github.com/go-playground/validator/v10"
     "go.mongodb.org/mongo-driver/bson/primitive"
     "go.mongodb.org/mongo-driver/mongo"
+	"github.com/gorilla/mux" //add this
+    "go.mongodb.org/mongo-driver/bson" //add this
 )
 
 var userCollection *mongo.Collection = configs.GetCollection(configs.DB, "users")
@@ -55,6 +57,30 @@ func CreateUser() http.HandlerFunc {
 
         rw.WriteHeader(http.StatusCreated)
         response := responses.UserResponse{Status: http.StatusCreated, Message: "success", Data: map[string]interface{}{"data": result}}
+        json.NewEncoder(rw).Encode(response)
+    }
+}
+
+func GetAUser() http.HandlerFunc {
+    return func(rw http.ResponseWriter, r *http.Request) {
+        ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+        params := mux.Vars(r)
+        userId := params["userId"]
+        var user models.User
+        defer cancel()
+
+        objId, _ := primitive.ObjectIDFromHex(userId)
+
+        err := userCollection.FindOne(ctx, bson.M{"id": objId}).Decode(&user)
+        if err != nil {
+            rw.WriteHeader(http.StatusInternalServerError)
+            response := responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}}
+            json.NewEncoder(rw).Encode(response)
+            return
+        }
+
+        rw.WriteHeader(http.StatusOK)
+        response := responses.UserResponse{Status: http.StatusOK, Message: "success", Data: map[string]interface{}{"data": user}}
         json.NewEncoder(rw).Encode(response)
     }
 }
