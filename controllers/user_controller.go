b8be815c16436f87affcc2390e18fa7a114de1a8
gkranasinghe <gkranasinghe@gmail.com>
2022-02-23 17:14:47 +0530

    add getalluser handlerfunc to user_controller

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -169,3 +169,37 @@ func DeleteAUser() http.HandlerFunc {
         json.NewEncoder(rw).Encode(response)
     }
 }
+
+func GetAllUser() http.HandlerFunc {
+    return func(rw http.ResponseWriter, r *http.Request) {
+        ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+        var users []models.User
+        defer cancel()
+
+        results, err := userCollection.Find(ctx, bson.M{})
+
+        if err != nil {
+            rw.WriteHeader(http.StatusInternalServerError)
+            response := responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}}
+            json.NewEncoder(rw).Encode(response)
+            return
+        }
+
+        //reading from the db in an optimal way
+        defer results.Close(ctx)
+        for results.Next(ctx) {
+            var singleUser models.User
+            if err = results.Decode(&singleUser); err != nil {
+                rw.WriteHeader(http.StatusInternalServerError)
+                response := responses.UserResponse{Status: http.StatusInternalServerError, Message: "error", Data: map[string]interface{}{"data": err.Error()}}
+                json.NewEncoder(rw).Encode(response)
+            }
+
+            users = append(users, singleUser)
+        }
+
+        rw.WriteHeader(http.StatusOK)
+        response := responses.UserResponse{Status: http.StatusOK, Message: "success", Data: map[string]interface{}{"data": users}}
+        json.NewEncoder(rw).Encode(response)
+    }
+}
